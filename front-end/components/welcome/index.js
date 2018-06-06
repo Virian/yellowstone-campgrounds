594ffee767cb0c9bc580c8ee12711673c7fb6112
Virian <gstok@poczta.fm>
2018-06-06 15:52:09 +0200

    Add information about timezone

--- front-end/components/welcome/index.js
+++ front-end/components/welcome/index.js
@@ -12,6 +12,7 @@ export default class Welcome extends React.Component {
         <h3>This website provides a history of fill times of campgrounds in Yellowstone National Park. For more detailed
           information about these campgrounds visit <a href="https://www.nps.gov/yell/planyourvisit/campgrounds.htm"
                                                        target="_blank">Yellowstone official site</a>.</h3>
+        <h3>All provided times are in Yellowstone local timezone - MDT (UTC-6)</h3>
       </div>
     )
   }
