5ccdd3b09df9e5f703ecc1028bbf8dd92bfa3853
Virian <gstok@poczta.fm>
2018-06-08 16:12:51 +0200

    Move datepicker to different component

--- front-end/components/calendar/index.js
+++ front-end/components/calendar/index.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import DatePicker from 'react-datepicker'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import FirebaseConfig from '../../config/firebase.config.json'
 import Campgrounds from '../../config/campgrounds.json'
 import moment from 'moment-timezone'
-import Button from '@material-ui/core/Button'
+import Datepicker from '../datepicker'
 import Table from '@material-ui/core/Table'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 import TableBody from '@material-ui/core/TableBody'
-import Icon from '@material-ui/core/Icon'
 
 import 'react-datepicker/dist/react-datepicker.css'
 
@@ -20,20 +18,14 @@ export default class Calendar extends React.Component {
     super(props)
     firebase.initializeApp(FirebaseConfig)
     this.db = firebase.firestore()
-    const settings = {timestampsInSnapshots: true}
+    const settings = { timestampsInSnapshots: true }
     this.db.settings(settings)
     moment.tz.setDefault('America/Denver');
     this.state = {
       startDate: moment(),
-      earliestDate: moment(),
-      latestDate: moment(),
       days: [],
       fillTimes: {}
     }
-    this.handleChange = this.handleChange.bind(this)
-    this.previousWeek = this.previousWeek.bind(this)
-    this.nextWeek = this.nextWeek.bind(this)
-    this.goToDate = this.goToDate.bind(this)
   }
 
   componentDidMount() {
@@ -64,8 +56,6 @@ export default class Calendar extends React.Component {
         })
         this.setState({
           fillTimes: fillTimes,
-          earliestDate: earliestDate,
-          latestDate: latestDate,
           days: days
         })
       })
@@ -74,56 +64,21 @@ export default class Calendar extends React.Component {
       })
   }
 
-  handleChange(date) {
+  setStartDate(startDate) {
     this.setState({
-      startDate: date
+      startDate: startDate
     })
-  }
-
-  previousWeek() {
-    this.setState({
-      startDate: this.state.startDate.subtract(1, 'week')
-    })
-    this.getFillTimes()
-  }
-
-  nextWeek() {
-    this.setState({
-      startDate: this.state.startDate.add(1, 'week')
-    })
-    this.getFillTimes()
-  }
-
-  goToDate() {
-    this.getFillTimes()
   }
 
   render() {
     const { fillTimes, days } = this.state
     return (
       <div className="calendar container">
-        <div className="datepicker row">
-          <DatePicker
-            selected={this.state.startDate}
-            onChange={this.handleChange}
-            maxDate={moment().tz('America/Denver')}
-            minDate={moment().year(2015).startOf('year')}
-            locale="en-gb"
-            showMonthDropdown
-            showYearDropdown
-            dropdownMode="select"
-            todayButton={'Today'}
-          />
-          <Button variant="contained" color="primary" className="btn-datepicker" onClick={this.goToDate}>Go to selected date</Button>
-          <Button variant="contained" color="primary" className="btn-datepicker" onClick={this.previousWeek}>
-            <Icon className="btn-datepicker-icon btn-datepicker-icon-left">arrow_back</Icon>
-            previous week
-          </Button>
-          <Button variant="contained" color="primary" className="btn-datepicker" onClick={this.nextWeek}>
-            next week
-            <Icon className="btn-datepicker-icon btn-datepicker-icon-right">arrow_forward</Icon>
-          </Button>
-        </div>
+        <Datepicker
+          startDate={this.state.startDate}
+          setStartDate={(startDate) => this.setStartDate(startDate)}
+          getFillTimes={() => this.getFillTimes()}
+        />
         <div className="row table-container">
           <Table>
             <TableHead>
---
+++ front-end/components/datepicker/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment-timezone'
+import Button from '@material-ui/core/Button'
+import Icon from '@material-ui/core/Icon'
+
+import 'react-datepicker/dist/react-datepicker.css'
+
+export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props)
+    moment.tz.setDefault('America/Denver');
+    this.handleChange = this.handleChange.bind(this)
+    this.previousWeek = this.previousWeek.bind(this)
+    this.nextWeek = this.nextWeek.bind(this)
+    this.goToDate = this.goToDate.bind(this)
+  }
+
+  handleChange(date) {
+    this.props.setStartDate(date)
+  }
+
+  previousWeek() {
+    this.props.setStartDate(this.props.startDate.subtract(1, 'week'))
+    this.props.getFillTimes()
+  }
+
+  nextWeek() {
+    this.props.setStartDate(this.props.startDate.add(1, 'week'))
+    this.props.getFillTimes()
+  }
+
+  goToDate() {
+    this.props.getFillTimes()
+  }
+
+  render() {
+    return (
+      <div className="datepicker row">
+        <DatePicker
+          selected={this.props.startDate}
+          onChange={this.handleChange}
+          maxDate={moment().tz('America/Denver')}
+          minDate={moment().year(2015).startOf('year')}
+          locale="en-gb"
+          showMonthDropdown
+          showYearDropdown
+          dropdownMode="select"
+          todayButton={'Today'}
+        />
+        <Button variant="contained" color="primary" className="btn-datepicker" onClick={this.goToDate}>Go to selected date</Button>
+        <Button variant="contained" color="primary" className="btn-datepicker" onClick={this.previousWeek}>
+          <Icon className="btn-datepicker-icon btn-datepicker-icon-left">arrow_back</Icon>
+          previous week
+        </Button>
+        <Button variant="contained" color="primary" className="btn-datepicker" onClick={this.nextWeek}>
+          next week
+          <Icon className="btn-datepicker-icon btn-datepicker-icon-right">arrow_forward</Icon>
+        </Button>
+      </div>
+    )
+  }
+}
