57d4d4f724210f4a53b95aa45cf21240c241082f
Virian <gstok@poczta.fm>
2018-06-13 13:16:14 +0200

    Remove precise info about timezone

--- front-end/components/welcome/index.js
+++ front-end/components/welcome/index.js
@@ -12,7 +12,7 @@ export default class Welcome extends React.Component {
         <h3>This website provides a history of fill times of campgrounds in Yellowstone National Park. For more detailed
           information about these campgrounds visit <a href="https://www.nps.gov/yell/planyourvisit/campgrounds.htm"
                                                        target="_blank">Yellowstone official site</a>.</h3>
-        <h4>All provided times are in Yellowstone local timezone - MDT (UTC-6). If there is no information about the
+        <h4>All provided times are in Yellowstone local timezone. If there is no information about the
           fill time on given day, it means that the campground either didn't fill or was closed.</h4>
         <h4>The website updates every midnight (Yellowstone local timezone). </h4>
       </div>
